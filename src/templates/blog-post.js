7af00c746ec78985fab8ef06dcc6a1648e95e1f1
unknown <marketing.KevinConti@gmail.com>
2019-06-10 07:15:56 -0400

    Fix form

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -90,11 +90,16 @@ export default function PageTemplate({ data: { mdx, site }, pageContext }) {
                 }
                 {!loading && !success &&
                   <div>
-                  <label>Name<input type='text' name='Name' required /></label>
-                    <br></br>
-                    <label>Email<input type='email' name='email' required /></label>
-                    <br />
-                    <button>Submit</button>
+                    <input type="hidden" name="form-name" value="contact" />
+                    <p>
+                      <label>Your Name: <input type="text" name="name"/></label>
+                    </p>
+                    <p>
+                      <label>Your Email: <input type="email" name="email"/></label>
+                    </p>
+                    <p>
+                      <button type="submit">Send</button>
+                    </p>
                   </div>
                 }
               </div>
